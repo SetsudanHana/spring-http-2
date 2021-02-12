5c812c1fdb6b2db2f6145531a6fb1b184f6b0080
Mateusz Wręczycki <setsudan.hana@gmail.com>
2021-02-12 23:26:12 +0100

    initial commit

---
+++ src/main/java/com/example/springhttp2/Springhttp2Application.java
@@ -0,0 +1,13 @@
+package com.example.springhttp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springhttp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springhttp2Application.class, args);
+	}
+
+}
